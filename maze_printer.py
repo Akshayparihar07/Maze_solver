c25bcafa7027ee9b94e11568cd639c65db9c5a22
Akshay Parihar <129431619+Akshayparihar07@users.noreply.github.com>
2024-01-06 01:54:08 +0530

    Update maze_printer.py

--- maze_printer.py
+++ maze_printer.py
@@ -1,9 +1,11 @@
 from maze_generator import generated_maze
 
 maze = generated_maze()
-size = len(maze)
 
-for i in range(size):
-    for j in range(size):
-        print(maze[i][j], end = " ")
-    print()
+def print_maze():
+    global maze
+    size = len(maze)
+    for i in range(size):
+        for j in range(size):
+            print(maze[i][j], end = " ")
+        print()
